a1104fa95f3d0b38c0645f50ccecd3ef55af5497
Drew Tempelmeyer <drewtemp@gmail.com>
2015-01-24 21:06:11 -0600

    Remove units variable

--- pi/temp-server/server.py
+++ pi/temp-server/server.py
@@ -10,11 +10,10 @@ from max31855 import MAX31855
 cs_pin    = 24
 clock_pin = 23
 data_pin  = 22
-units     = "f"
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        thermocouple = MAX31855(cs_pin, clock_pin, data_pin, units)
+        thermocouple = MAX31855(cs_pin, clock_pin, data_pin, 'f')
         thermocouple.read()
         celsius     = thermocouple.data_to_tc_temperature()
         farenheight = thermocouple.to_f(celsius)
